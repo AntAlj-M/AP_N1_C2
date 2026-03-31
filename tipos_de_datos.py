2f5801ce5b35bed30760286a1f4bd30844f6ade6
AntAlj-M <antonioalejandromm12@gmail.com>
2026-03-31 10:53:42 -0300

    Primera carga de codigo

---
+++ tipos_de_datos.py
@@ -0,0 +1,13 @@
+# Tipos de datos en python
+
+numero_real = 0
+numero_decimal = 0.0
+cadena_de_texto = "Buneas estimado"
+
+print("No mi primer mensaje en python")
+print(numero_real)
+print(type(numero_real))
+print(numero_decimal)
+print(type(numero_decimal))
+print(cadena_de_texto)
+print(type(cadena_de_texto))
