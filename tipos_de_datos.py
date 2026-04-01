a0e3e10d679175b906ec22b33b05023bb5d49b11
AntAlj-M <antonioalejandromm12@gmail.com>
2026-04-01 10:53:02 -0300

    clase 01/04/26

--- tipos_de_datos.py
+++ tipos_de_datos.py
@@ -1,13 +1,61 @@
 # Tipos de datos en python
 
-numero_real = 0
+print(type(5))
+print(type(3.2))
+print(type("hola"))
+print(type(True))
+
+numero_entero = 0
 numero_decimal = 0.0
 cadena_de_texto = "Buneas estimado"
+valor_booleano = False
 
 print("No mi primer mensaje en python")
-print(numero_real)
-print(type(numero_real))
+
+print()
+print(numero_entero)
+print(type(numero_entero))
+
+print()
 print(numero_decimal)
 print(type(numero_decimal))
+
+print()
 print(cadena_de_texto)
 print(type(cadena_de_texto))
+
+print()
+print(valor_booleano)
+print(type(valor_booleano))
+
+valor_booleano = 5 > 3
+print()
+print(valor_booleano)
+print(type(valor_booleano))
+
+#Concatenación de cadenas de texto
+saludo = "Buen dia master,¿me deposita 5 lucas?"
+nombre = " Antonio necesita 5 lucas"
+print()
+print(saludo + nombre)
+
+texto_de_una_linea_comillas_dobles = "texto de una linea"
+texto_de_una_linea_comillas_simples = 'texto de una linea'
+
+texto_de_mulriples_lineas_comillas_dobles = """
+Linea 1 texto multiples con dobles comillas
+Linea 2 texto multiples con dobles comillas
+Linea 3 texto multiples con dobles comillas
+Linea 4 texto multiples con dobles comillas
+"""
+texto_de_multiples_lineas_comillas_simples = '''
+Linea 1 texto multiples con comillas simples
+Linea 2 texto multiples con comillas simples
+Linea 3 texto multiples con comillas simples
+Linea 4 texto multiples con comillas simples
+'''
+
+print(texto_de_una_linea_comillas_dobles)
+print(texto_de_una_linea_comillas_simples)
+print(texto_de_mulriples_lineas_comillas_dobles)
+print(texto_de_multiples_lineas_comillas_simples)
