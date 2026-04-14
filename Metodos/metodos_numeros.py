c2b5996b5f64ae0eefc0ebf42f16c16b369efd67
AntAlj-M <antonioalejandromm12@gmail.com>
2026-04-14 10:51:46 -0400

    14/04/26

---
+++ Metodos/metodos_numeros.py
@@ -0,0 +1,33 @@
+import math
+from math import pi as pi
+
+primer_numero = 45
+segundo_numero = 15
+
+#Operaciones ARITMETICAS
+print(primer_numero + segundo_numero)
+print(primer_numero - segundo_numero)
+print(primer_numero * segundo_numero)
+print(primer_numero / segundo_numero)
+
+print(dir(primer_numero))
+#is_integer esponde true si el número tiene una parte entera
+print(primer_numero.is_integer())
+divison = 9 / 5
+print(9/5)
+print(divison.is_integer())
+#Responde false si el número tiene un valor decimal}
+
+
+#trunca el resultado de una operación o número
+print(math.floor(9/5))
+#Aproxima el resultado de una operaci+on o número
+print(round(9/5))
+#Calcula la raiz cuadrada de un número
+print(math.sqrt(9))
+#Eleva el número e a la potencia indicada como argumento
+print(math.exp(5)) #?????
+
+print(math.pi * 25)
+
+#print(dir(match))
