840050b301305fb852616f09873e7895c80d216f
AntAlj-M <antonioalejandromm12@gmail.com>
2026-04-29 10:47:55 -0400

    29/04

--- Funciones/crear_funciones.py
+++ Funciones/crear_funciones.py
@@ -4,33 +4,41 @@
 def suma(num_1,num_2):
     #Este sera el contenido de la función
     resultado = num_1 + num_2
-    print(f'{num_1} + {num_2} = {resultado}')
+    return resultado
 
 def resta(num_1,num_2):
     resultado = num_1 - num_2
-    print(f'{num_1} - {num_2} = {resultado}')
+    return resultado
 
 def mult(num_1,num_2):
     resultado = num_1 * num_2
-    print(f'{num_1} X {num_2} = {resultado}')
+    return resultado
 
 def div(num_1,num_2):
      if num_2 == 0:
          print("No se puede dividir en 0")
      else:
          resultado = num_1 / num_2
-         print(f'{num_1} / {num_2} = {resultado}')
+         return resultado
 
 def pedir_datos():
     num_1 = input('Ingrese el primer número: ')
     num_2 = input('Ingrese el segundo número: ')
        
-    if num_1.isdigit() and num_2.isdigit():
-        num_1 = float (num_1)
-        num_2 = float (num_2)
+    num_1 = convertir_float(num_1)
+    num_2 = convertir_float(num_2)
+    
+    if num_1 and num_2 != False:
+        return(num_1,num_2)
     else:
         print("Ingrese un número valido")
     return(num_1,num_2)
+
+def convertir_float(valor):
+    try:
+        return float(valor)
+    except(ValueError, TypeError):
+        return False
 
 print()
 print('Bienvenido a mi segunda calculadora')
@@ -43,21 +51,33 @@ while ciclo == True:
     print('[3] multiplicación')
     print('[4] división')
     print('[0] salir')
-    opciom = input('\n Seleccione su operación [0-4] : ')
+    opcion = input('\nSeleccione su operación [0-4]: ')
 
-    if opciom == '0':
-        ciclo = False
-        print('Gracias por usar mi calculadora')
-        print('Saliendo...')
-    
-    a,b = pedir_datos()
-    if opciom == '1':
-        suma(a,b)
-    elif opciom == '2':
-        resta(a,b)
-    elif opciom == '3':
-        mult(a,b)
-    elif opciom == '4':
-        div(a,b)
+    opciones_validas = ['0','1','2','3','4']
+
+    if opcion in(opciones_validas):
+        if opcion == '0':
+            ciclo = False
+            print('Gracias por usar mi calculadora!')
+            print('Saliendo...')
+        else:
+            a,b = pedir_datos()
+            operacion = ''
+            if a and b != False:
+                if opcion == '1':
+                    operacion = '+'
+                    valor = suma(a,b)
+                elif opcion == '2':
+                    operacion = '-'
+                    valor = resta(a,b)
+                elif opcion == '3':
+                    operacion = 'x'
+                    valor = mult(a,b)
+                elif opcion == '4':
+                    operacion = '/'
+                    valor = div(a,b)
+                print(f'{a} {operacion} {b} = {valor}')
+            else:
+                print('Valor no corresponde')
     else:
-        print('Elija una opción valida')
+        print('Opción NO válida.')
