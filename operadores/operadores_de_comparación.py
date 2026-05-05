738519e99d1e4f977b453c6d73ef02e76982576e
AntAlj-M <antonioalejandromm12@gmail.com>
2026-05-05 10:48:03 -0400

    05/05

---
+++ operadores/operadores_de_comparación.py
@@ -0,0 +1,25 @@
+# Operador EQUIVALENTE
+print('Operador EQUIVALENTE')
+print(f'5 == 6? {5 == 6}')
+print(f'3 == 9/3? {3 == 9/3}')
+
+# Operador MAYOR QUE
+print(f'6 > 9? {6 > 9}')
+print(f'0 > 3? {0 > 3}')
+
+# Operador MAYOR O IGUAL QUE
+print(f'6 >= 9? {6 >= 9}')
+print(f'6 >= 12/2? {6 >= 12/2}')
+
+# Operador MENOR QUE
+print(f'6 < 9? {6 < 9}')
+print(f'0 < 3? {0 < 3}')
+
+# Operador MENOR O IGUAL QUE
+print(f'6 <= 9? {6 <= 9}')
+print(f'6 <= 12/2? {6 <= 12/2}')
+
+
+# Operador DISTINTO
+print('\nOperador DISTINTO')
+print(f'5 != ')
